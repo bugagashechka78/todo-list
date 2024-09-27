0bd4bb522a339023c837adb5c507232b3b25bc69
bugagashechka78 <bugagashechka78@gmail.com>
2024-09-27 10:12:28 +0300

    pre-pre-alpha

---
+++ src/components/TodoList.vue
@@ -0,0 +1,36 @@
+<template>
+  <n-flex justify="space-around" size="large">
+    <n-flex vertical>
+      <h2>Список дел</h2>
+      <n-button size="large" @click="showModal = true">Добавить дело</n-button>
+      <n-card
+          v-for="todo in todos"
+          :title='todo.title'
+          :key='todo.id'
+      >
+        {{ todo.description }}
+      </n-card>
+    </n-flex>
+  </n-flex>
+  <TodoModal v-model="showModal"/>
+</template>
+
+<script setup lang="ts">
+import {ref} from 'vue'
+import {NFlex, NCard, NButton} from 'naive-ui'
+import TodoModal from '@/components/TodoModal'
+
+const showModal = ref(false);
+
+
+interface Props {
+  todos: [];
+}
+
+defineProps<Props>()
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/TodoModal.vue
@@ -0,0 +1,32 @@
+<template>
+  <n-modal v-model:show="showModal">
+    <n-card
+        style="width: 600px"
+        title="Modal"
+        :bordered="false"
+        size="huge"
+        role="dialog"
+        aria-modal="true"
+    >
+      <n-flex vertical>
+        <n-input value="valueTitle" type="text" placeholder="Basic Input" />
+        <n-input
+            value="valueDescription"
+            type="textarea"
+            placeholder="Basic Textarea"
+        />
+      </n-flex>
+    </n-card>
+  </n-modal>
+</template>
+
+<script setup lang="ts">
+import {NFlex, NCard, NModal, NInput} from 'naive-ui'
+
+const showModal = defineModel();
+
+</script>
+
+<style scoped>
+
+</style>
