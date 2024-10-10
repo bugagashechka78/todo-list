7f14fbf7a8225ebfca7f8ec1b82e4805062e4040
bugagashechka78 <bugagashechka78@gmail.com>
2024-10-10 12:05:12 +0300

    Todo List CLI

--- src/components/TodoModal.vue
+++ src/components/TodoModal.vue
@@ -1,19 +1,29 @@
 <template>
-  <n-modal v-model:show="showModal">
+  <n-modal
+      style="width: 600px"
+      v-model:show="modalData.showModal"
+      preset="dialog"
+      v-model:title="modalData.modalTitle"
+      positive-text="Сохранить"
+      negative-text="Удалить дело"
+      @positive-click="saveClick(modalData.todoId, modalData.todoTitle, modalData.todoDescription, modalData.isNew)"
+      @negative-click="removeClick(modalData.todoId, modalData.isNew)"
+  >
+    <n-alert v-if="showAlert" title="Пустое дело" type="warning" closable>
+      Заполните название дела
+    </n-alert>
     <n-card
-        style="width: 600px"
-        title="Modal"
         :bordered="false"
         size="huge"
         role="dialog"
         aria-modal="true"
     >
       <n-flex vertical>
-        <n-input value="valueTitle" type="text" placeholder="Basic Input" />
+        <n-input v-model:value="modalData.todoTitle" type="text" placeholder="Дело"/>
         <n-input
-            value="valueDescription"
+            v-model:value="modalData.todoDescription"
             type="textarea"
-            placeholder="Basic Textarea"
+            placeholder="Как делать"
         />
       </n-flex>
     </n-card>
@@ -21,9 +31,38 @@
 </template>
 
 <script setup lang="ts">
-import {NFlex, NCard, NModal, NInput} from 'naive-ui'
+import {ref} from 'vue';
+import {NFlex, NCard, NModal, NInput, NAlert, useMessage} from 'naive-ui'
+import {useTodosStore, ModalData} from '@/stores/TodosStore';
 
-const showModal = defineModel();
+const store = useTodosStore()
+
+const modalData = defineModel<ModalData>();
+
+const showAlert = ref(false);
+const message = useMessage()
+
+
+function saveClick(todoId: number, valueTitle: string, valueDescription: string, isNew: boolean) {
+  if (valueTitle === "" && valueDescription === "") {
+    message.warning('Нельзя создать пустое дело')
+  } else {
+    if (isNew) {
+      store.addTodo(todoId, valueTitle, valueDescription);
+    } else {
+      store.editTodo(todoId, valueTitle, valueDescription);
+    }
+  }
+}
+
+function removeClick(todoId: number, isNew: boolean) {
+  if (!isNew) {
+    store.removeTodo(todoId);
+  } else {
+    message.warning('Нельзя удалить несуществующее дело')
+  }
+}
+
 
 </script>
 
